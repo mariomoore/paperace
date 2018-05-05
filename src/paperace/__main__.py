1cd0c59ecf8911a1185ff82a504e8de932b8c0e0
Mariusz Murawski <mariusz.murawski84@gmail.com>
2018-05-05 23:53:00 +0200

    dodanie gracza B (zielonego) i metody get_allowed_car_positions()

--- src/paperace/__main__.py
+++ src/paperace/__main__.py
@@ -37,6 +37,15 @@ def check_crossing_lines(first_img, second_img, rectbegin, rectend, first_color,
                 return True
     return False
 
+def get_allowed_car_positions(cars, i):
+    """Zwraca listę punktów, na które może przemieścić się samochód cars[i] oraz usuwa pozycje, na których znajduje się pojazd przeciwnika)."""
+    positions = cars[i].get_allowed_positions()
+    for c in cars:
+        position_to_remove = ((c.xpos, c.ypos))
+        if position_to_remove in positions:
+            positions.remove(position_to_remove)
+    return positions
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -45,9 +54,17 @@ def main():
     imgfile = os.path.join(os.path.split(__file__)[0], 'plansza\plansza.png')
     background = pygame.image.load(imgfile)
 
-    a = Car(400, 80, BLUE) # Pojazd gracza A
+    a = Car(400, 60, BLUE) # Pojazd gracza A
+    b = Car(400, 120, GREEN) # Pojazd gracza B
 
-    pygame.draw.circle(background, a.color, [a.xpos, a.ypos], 5)
+    cars = [a, b] # Lista pojazdów
+    cp = 0 # Aktualny gracz na liście. Current Player
+
+    screen.blit(background, background.get_rect())
+    pygame.draw.circle(screen, a.color, [a.xpos, a.ypos], 5)
+    pygame.draw.circle(screen, b.color, [b.xpos, b.ypos], 5)
+    pygame.draw.rect(screen, a.color, (0, 0, WIDTH, HEIGHT) , 3)
+    pygame.display.flip()
 
     run = True
     while run:
@@ -56,22 +73,27 @@ def main():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 nearestpoint = get_nearest_point(pygame.mouse.get_pos())
-                if nearestpoint in a.get_allowed_positions():
-                    xoldpos = a.xpos
-                    yoldpos = a.ypos
-                    a.move(nearestpoint)
-                    pygame.draw.line(background, a.color, [xoldpos, yoldpos], [a.xpos, a.ypos], 2)
-                    if check_crossing_lines(screen, background, (a.xpos, a.ypos), (xoldpos, yoldpos), BLACK, a.color):
+                if nearestpoint in get_allowed_car_positions(cars, cp):
+                    xoldpos = cars[cp].xpos
+                    yoldpos = cars[cp].ypos
+                    cars[cp].move(nearestpoint)
+                    pygame.draw.line(screen, cars[cp].color, [xoldpos, yoldpos], [cars[cp].xpos, cars[cp].ypos], 2)
+                    if check_crossing_lines(background, screen, (cars[cp].xpos, cars[cp].ypos), (xoldpos, yoldpos), BLACK, cars[cp].color):
                         print("Koniec gry! Kraksa")
                         run = False
-                    if check_crossing_lines(screen, background, (a.xpos, a.ypos), (xoldpos, yoldpos), RED, a.color):
+                    if check_crossing_lines(background, screen, (cars[cp].xpos, cars[cp].ypos), (xoldpos, yoldpos), RED, cars[cp].color) and xoldpos != 400:
                         print("Koniec gry! ZWYCIĘSTWO!")
                         run = False
-                    if len(a.get_allowed_positions()) == 0:
+                    if len(cars[cp].get_allowed_positions()) == 0:
                         print("Koniec gry! Następny ruch poza pole")
                         run = False
-                    pygame.draw.circle(background, a.color, [a.xpos, a.ypos], 5)
-        screen.blit(background, background.get_rect())
+                    pygame.draw.circle(screen, cars[cp].color, [cars[cp].xpos, cars[cp].ypos], 5)
+                    
+                    cp = cp + 1
+                    if cp >= len(cars):
+                        cp = 0
+
+                    pygame.draw.rect(screen, cars[cp].color, (0, 0, WIDTH, HEIGHT) , 3)
         pygame.display.flip()
 
 if __name__ == '__main__' :
