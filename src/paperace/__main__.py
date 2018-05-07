e23bba05aa1a065ab37a66f330f8f571d6abf36c
Mariusz Murawski <mariusz.murawski84@gmail.com>
2018-05-07 21:51:55 +0200

    Dodanie: kolorów, metody init_cars_rand() i testów

--- src/paperace/__main__.py
+++ src/paperace/__main__.py
@@ -2,8 +2,22 @@ import sys
 import os
 import pygame
 import math
+import random
 from paperace.car import Car
 from paperace.const import *
+
+def init_cars_rand(players):
+    """Zwraca listę pojazdów cars, w zależności od liczby graczy (players). Pojazdy ustawia losowo na linii ([400, 40], [400, 140])."""
+    cars = []
+    colors = [RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA]
+    y_positions = [40, 60, 80, 100, 120, 140]
+    for _ in range(players):
+        y = y_positions[random.randint(0, len(y_positions)-1)]
+        y_positions.remove(y)
+        color = colors[random.randint(0, len(colors)-1)]
+        colors.remove(color)
+        cars.append(Car(400, y, color))
+    return cars
 
 def get_nearest_point(mousexy):
     """Zwraca punkt najbliższego przecięcia kratek po kliknięciu myszką.""" 
@@ -21,8 +35,8 @@ def check_crossing_lines(first_img, second_img, rectbegin, rectend, first_color,
     """Zwraca True jeśli na prostokątnym obszarze rectbegin x rectend na obrazkach first_img i second_img przecinają się linie o kolorach odpowiednio: first_color i second_color."""
     # Ustawaia prawidłowy prostokąt
     xbegin = min(rectbegin[0], rectend[0])
-    xend = max(rectbegin[0], rectend[0])
     ybegin = min(rectbegin[1], rectend[1])
+    xend = max(rectbegin[0], rectend[0])
     yend = max(rectbegin[1], rectend[1])
     if xbegin == xend:
         xend = xend + 1
@@ -52,18 +66,15 @@ def main():
     pygame.display.set_caption("paperace - my first python game")
 
     imgfile = os.path.join(os.path.split(__file__)[0], 'plansza\plansza.png')
-    background = pygame.image.load(imgfile)
+    background = pygame.image.load(imgfile).convert()
 
-    a = Car(400, 60, BLUE) # Pojazd gracza A
-    b = Car(400, 120, GREEN) # Pojazd gracza B
-
-    cars = [a, b] # Lista pojazdów
-    cp = 0 # Aktualny gracz na liście. Current Player
+    cars = init_cars_rand(2) # Lista pojazdów
+    cp = 0 # Aktualny gracz na liście - Current Player
 
     screen.blit(background, background.get_rect())
-    pygame.draw.circle(screen, a.color, [a.xpos, a.ypos], 5)
-    pygame.draw.circle(screen, b.color, [b.xpos, b.ypos], 5)
-    pygame.draw.rect(screen, a.color, (0, 0, WIDTH, HEIGHT) , 3)
+    for car in cars:
+        pygame.draw.circle(screen, car.color, [car.xpos, car.ypos], 5)
+    pygame.draw.rect(screen, cars[0].color, (0, 0, WIDTH, HEIGHT) , 3)
     pygame.display.flip()
 
     run = True
--- tests/test_paperace.py
+++ tests/test_paperace.py
@@ -2,6 +2,7 @@ import pytest
 from paperace.__main__ import get_nearest_point
 import pygame
 from paperace.__main__ import check_crossing_lines
+from paperace.__main__ import init_cars_rand
 import os
 import sys
 
@@ -20,9 +21,17 @@ def test_get_nearest_point(mousexy, answerxy):
     ((0, 0), (10, 10), (0, 0, 0, 255), (0, 0, 255, 255), True), # BLACK, BLUE
     ((10, 0), (20, 10), (0, 0, 0, 255), (0, 0, 255, 255), False), # BLACK, BLUE
     ((20, 0), (30, 10), (255, 0, 0, 255), (0, 0, 255, 255), True), # RED, BLUE
+    ((0, 0), (10, 0), (0, 0, 0, 255), (0, 0, 255, 255), True), # BLACK, BLUE
+    ((0, 0), (0, 10), (0, 0, 0, 255), (0, 0, 255, 255), True), # BLACK, BLUE
 ])
 def test_check_crossing_lines(begin, end, colorscr, colorbgd, answer):
     """Sprawdza czy metoda prawidłowo wskazuje przecięte linie o zadanych kolorach"""
     screen = pygame.image.load(os.path.join(sys.path[0], 'poziome.png'))
     background = pygame.image.load(os.path.join(sys.path[0], 'pionowe.png'))
     assert check_crossing_lines(screen, background, begin, end, colorscr, colorbgd) == answer
+
+@pytest.mark.parametrize('players', [0, 1, 2, 6])
+def test_init_cars_rand(players):
+    """Sprawdza, czy metoda prawidłowo inicjalizuje pojazdy o ilości players"""
+    cars = init_cars_rand(players) # lista samochodów
+    assert len(cars) == players
